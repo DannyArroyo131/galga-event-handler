98b2dc81cd86d034301daa557841e896d0829af0
DannyArroyo131 <100878741+DannyArroyo131@users.noreply.github.com>
2022-03-04 14:37:30 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(img`
         . . . . . . . . . . . . . . . . 
@@ -78,24 +83,26 @@ forever(function () {
 })
 game.onUpdateInterval(500, function () {
     Alien = sprites.create(img`
-        . . . . . . . . c c c c . . . . 
-        . . . . c c c c c c c c c . . . 
-        . . . c f c c a a a a c a c . . 
-        . . c c f f f f a a a c a a c . 
-        . . c c a f f c a a f f f a a c 
-        . . c c a a a a b c f f f a a c 
-        . c c c c a c c b a f c a a c c 
-        c a f f c c c a b b 6 b b b c c 
-        c a f f f f c c c 6 b b b a a c 
-        c a a c f f c a 6 6 b b b a a c 
-        c c b a a a a b 6 b b a b b a . 
-        . c c b b b b b b b a c c b a . 
-        . . c c c b c c c b a a b c . . 
-        . . . . c b a c c b b b c . . . 
-        . . . . c b b a a 6 b c . . . . 
-        . . . . . . b 6 6 c c . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . 7 . . . . . . . . . . . . . . 
+        . 7 7 . . . . . . . . . 7 7 . . 
+        . . 7 7 7 . . . . . . 7 . 7 . . 
+        . . . . 7 7 7 7 7 7 7 . . . . . 
+        . . . . . 7 f 7 f 7 . . . . . . 
+        . . . . . 7 f 7 f 7 . . . . . . 
+        . . . . . 7 7 f 7 7 . . . . . . 
+        . . . . . 7 7 f 7 7 . . . . . . 
+        . . . . 7 7 7 7 7 7 7 7 . . . . 
+        . . . 7 . . . . . . . 7 7 . . . 
+        . 7 7 . . . . . . . . . 7 7 . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
         `, SpriteKind.Enemy)
     Alien.setVelocity(-100, 0)
     Alien.setPosition(160, 0)
     Alien.setPosition(160, randint(0, 120))
+    scaling.scaleByPixels(Alien, randint(20, 50), ScaleDirection.Horizontally, ScaleAnchor.Middle)
+    scaling.scaleToPixels(Alien, randint(20, 50), ScaleDirection.Vertically, ScaleAnchor.Middle)
 })
